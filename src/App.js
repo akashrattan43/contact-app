4f9d1823c3564863763349c7912b051817ae2fda
akash rattan <akashrattan43@Gmail.com>
2020-09-14 18:06:54 -0700

    format done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header branding='Contact Manager'/>
+    <div className='container'>
+    <Contact name='John' email='John@email.com' phone='555-514-5555'/>
+    <Contact name='Doe' email='Doe@email.com' phone='515-554-5485'/>
+    </div>
     </div>
   );
 }
